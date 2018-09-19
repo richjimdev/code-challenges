6ec9f79201ae4ccea0d53d6e7d37df6fbe503f81
richjimdev <richjimdev@gmail.com>
2018-09-18 17:32:21 -0700

    ch 6

--- 12-sort/challenges-12.test.js
+++ 12-sort/challenges-12.test.js
@@ -91,7 +91,9 @@ const sortByPrice = (objs) => {
 // ------------------------------------------------------------------------------------------------
 
 const sortNumbersByLength = (nums) => {
-  // Solution code here...
+  return nums.sort( (a, b) => {
+    return a.toString().length - b.toString().length;
+  });
 };
 
 // -----------------------------------------------------------------------------------------------
